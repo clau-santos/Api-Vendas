e84f96e0414a9711e3597a082b0c6bc0773f1833
Cláudia Santos <claudia.santos1996@hotmail.comgit congit --global user.name clau-santosrun git config --global user.email claudia.santos1996@hotmail.com>
2021-03-31 11:42:58 -0400

    Primeiro commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '95531fbecdbf30ad9b5de9d7d6ea97e8',
+    expiresIn: '1d',
+  },
+};
---
+++ src/config/mail/EtherealMail.ts
@@ -0,0 +1,61 @@
+import nodemailer from 'nodemailer';
+import HandlebarsMailTemplate from './HandlebarsMailTemplate';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+interface ITemplateVariable {
+  [key: string]: string | number; //variavel dinamica
+}
+
+interface IParseMailTemplate {
+  template: string;
+  variables: ITemplateVariable;
+}
+
+interface ISendMail {
+  to: IMailContact;
+  from?: IMailContact; // ? indica que é opcional
+  subject: string;
+  templateData: IParseMailTemplate;
+}
+
+export default class EtherealMail {
+  static async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMail): Promise<void> {
+    const account = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
+      },
+    });
+
+    const mailTemplate = new HandlebarsMailTemplate();
+    const message = await transporter.sendMail({
+      from: {
+        name: from?.name || 'Equipe API Vendas', // || indica que caso não tenha um  nome, o padrão "equipe..." será enviado
+        address: from?.email || 'equipe@apivendas.com.br', // address é do nodemail
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await mailTemplate.parse(templateData),
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
