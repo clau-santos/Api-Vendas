64350710adf0592d4fd4b0e65c33d9dc22de704c
Cláudia Santos <claudia.santos1996@hotmail.comgit congit --global user.name clau-santosrun git config --global user.email claudia.santos1996@hotmail.com>
2021-04-04 18:12:27 -0400

    Pagination

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(pagination);
+
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
@@ -24,6 +27,7 @@ app.use(
         message: error.message,
       });
     }
+    console.log(error);
     return response.status(500).json({
       status: 'error',
       message: 'Internal server error',
